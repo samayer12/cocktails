3d443ad6e188e75abfacdade1cbd4a0f21fc1557
Sam Mayer <samayer12@gmail.com>
2021-12-02 15:38:19 -0600

    Basic parsing of .yml file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pandas as pd
+from yaml import SafeLoader, load
 
 
 def main():
-    pass
+    path_to_yml = 'recipes/apple_martini.yml'
+    with open(path_to_yml) as yml_file:
+        yml_contents = load(yml_file, Loader=SafeLoader)
+    df_cocktails = pd.json_normalize(yml_contents)
+    print(df_cocktails)
 
 
 if __name__ == "__main__":
