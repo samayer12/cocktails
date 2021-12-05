42ef8d012e1af3602fcb92674a24b5a468b07475
Sam Mayer <samayer12@gmail.com>
2021-12-04 18:45:30 -0600

    Parameterize input and add logging

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import argparse
+import glob
+import logging
+import time
+
 import pandas as pd
 from yaml import SafeLoader, load
 
@@ -42,15 +47,37 @@ def print_yield(yields: pd.Series) -> str:
 
 
 def main():
-    path_to_yml = 'recipes/apple_martini.yml'
-    with open(path_to_yml) as yml_file:
-        yml_contents = load(yml_file, Loader=SafeLoader)
-    df_cocktails = pd.json_normalize(yml_contents)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('recipe_directory', type=str)
+    parser.add_argument('log_file', type=str)
+    args = parser.parse_args()
+
+    logging.basicConfig(filename=f'log/{args.log_file}', level=logging.INFO, force=True,
+                        format='%(asctime)s, %(levelname)s, %(name)s, %(message)s')
+    logging.info('Start')
+    logging.info('Processing recipe data')
+
+    start_time = time.perf_counter()
+
+    recipe_files = glob.glob(f'{args.recipe_directory}/*.yml')
+    df_cocktails = pd.DataFrame()
+
+    for file in recipe_files:
+        with open(file) as yml_file:
+            yml_contents = load(yml_file, Loader=SafeLoader)
+        df_cocktails = pd.json_normalize(yml_contents)
+
     print(df_cocktails)
     print(df_cocktails.recipe_name.values)
     print(print_ingredients(df_cocktails.ingredients))
     print(print_steps(df_cocktails.steps))
     print(print_yield(df_cocktails.yields))
+
+    end_time = time.perf_counter()
+    total = end_time - start_time
+
+    logging.info('Processing recipe data complete in %s seconds', str(total))
+    logging.info('End')
 
 
 if __name__ == "__main__":
