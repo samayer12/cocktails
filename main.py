f001a1811a8a267461dbfa07ba8566a2bcb75f50
Sam Mayer <samayer12@gmail.com>
2021-12-04 19:14:58 -0600

    Iteratively populate dataframe from directory

--- main.py
+++ main.py
@@ -65,14 +65,14 @@ def main():
     for file in recipe_files:
         with open(file) as yml_file:
             yml_contents = load(yml_file, Loader=SafeLoader)
-        df_cocktails = pd.json_normalize(yml_contents)
+        df_cocktails = pd.concat([df_cocktails, pd.json_normalize(yml_contents)], ignore_index=True)
 
     print(df_cocktails)
     output_example = df_cocktails.loc[df_cocktails['recipe_uuid'] == 'dec34561-fa91-4fe9-a77c-51cf333e9d60']
-    print(output_example.recipe_name[0])
-    print(print_ingredients(output_example.ingredients))
-    print(print_steps(output_example.steps))
-    print(print_yield(output_example.yields))
+    print(output_example.recipe_name.values[0])
+    print(print_ingredients(output_example.ingredients.values))
+    print(print_steps(output_example.steps.values))
+    print(print_yield(output_example.yields.values))
 
     end_time = time.perf_counter()
     total = end_time - start_time
