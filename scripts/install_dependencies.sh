d9201abc1486cf2b19b7f5a7d00378087100f045
Sam Mayer <samayer12@gmail.com>
2021-12-23 16:18:35 -0600

    Install Python 3.8

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,7 @@
 yum -y update
 yum install -y httpd
 yum install -y aws-cli
-./setup
+amazon-linux-extras install python3.8
 cd /home/ec2-user || return
 if [[ ! -f ./install ]]; then
   wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
