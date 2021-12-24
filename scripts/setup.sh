83bd0f938445f08f35241342767d598d669287a3
Sam Mayer <samayer12@gmail.com>
2021-12-24 16:30:17 -0600

    Don't attempt --user pip install inside a venv

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd /app
-python3.8 -m pip3 install --user virtualenv
+python3.8 -m pip install --user virtualenv
 virtualenv venv --python=python3.8
 source venv/bin/activate
-pip3 install --user -r requirements.txt
+pip install -r requirements.txt
 mkdir -p /app/out
 mkdir -p /app/log
 chown -R ec2-user:ec2-user /app
