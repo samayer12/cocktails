939d5f713a8686f015332655460fddc7e0750d3c
Sam Mayer <samayer12@gmail.com>
2021-12-27 14:02:09 -0600

    Make ExecStart run start_server.sh, delete tutorial project relics

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-service httpd start
-service httpd enable
-
-# User perms 
-usermod -a -G apache ec2-user
-groups
-chown -R ec2-user:apache /var/www
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} \;
-find /var/www -type f -exec chmod 0664 {} \;
+/app/venv/bin/python3.8 main.py recipes log.txt
