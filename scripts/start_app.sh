61988fc978017b93ed5c066620de4539e6262269
Sam Mayer <samayer12@gmail.com>
2021-12-27 15:34:03 -0600

    Stop active service, if present

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp /app/cocktails.service /etc/systemd/system/cocktails.service
 systemctl daemon-reload
+systemctl stop cocktails
 systemctl start cocktails
 systemctl enable cocktails
 systemctl status cocktails
