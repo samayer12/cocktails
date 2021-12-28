7ec68aa96e4dcd1f27d6ec20e0816c2bd7b82433
Braden Laverick <blav62@gmail.com>
2021-12-28 12:33:50 -0600

    Changed setup script to allow python3 and added refresh button

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,8 @@
 
 cd app/ 2>/dev/null || true
 echo "Creating virtual environment in: $(pwd)"
-python3.8 -m pip install --user virtualenv
-virtualenv venv --python=python3.8
+python3 -m pip install --user virtualenv
+virtualenv venv --python=python3
 source venv/bin/activate
 pip install -r requirements.txt
-echo "If running locally, try sudo -E venv/bin/python3.8 src/main.py recipes log.txt"
+echo "If running locally, try sudo -E venv/bin/python3 src/main.py recipes log.txt"
